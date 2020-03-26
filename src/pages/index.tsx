8c3b73619dc0ef739e74f81dd48234a04f355258
Robert Osborne <robertosborne@Roberts-Mac-mini.local>
2020-03-25 21:35:20 -0700

    Add message

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,9 @@ export default () => (
     <Layout>
         <div className="container ph4 sans-serif">
             <h1 className="f1 lh-title tc white">Todo List</h1>
+            <p className="center f4 tc w-50 white">
+                Please don't store important tasks. All items will be deleted.
+            </p>
 
             <Todos />
         </div>
